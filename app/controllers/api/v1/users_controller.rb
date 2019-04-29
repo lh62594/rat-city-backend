3f0660c3a3120230d9987d55bfe291aee9c1b5a4
Linya Hu <lh62594@yahoo.com>
2019-04-29 13:26:14 -0400

    finished backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    @users = User.all.sort_by{|user| user.score}.reverse
+    @users = User.all.sort_by{|user| user.score}.reverse[0..9]
     render json: @users
   end
 
   def create
     @user = User.create(user_params)
+    render json: @user
   end
 
   private
