19e34988aa68a230271134f0c6a2f7e5ab736c26
Linya Hu <lh62594@yahoo.com>
2019-04-30 15:58:36 -0400

    edit cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ratcity.herokuapp.com/'
+    origins 'ratcity.herokuapp.com/'
 
     resource '*',
       headers: :any,
